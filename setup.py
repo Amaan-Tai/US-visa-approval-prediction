099ae7daa786bf8ff15be920de5f6ee15c68e3f1
Amaan-Tai <amaantai279@gmail.com>
2026-03-06 11:36:04 +0530

    chore: add project dependencies and packaging configuration

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "us_visa_approval_prediction",
+    version = "1.0.0",
+    author = "Amaan Tai",
+    author_email= "amaantai279@gmail.com",
+    packages= find_packages()
+)
